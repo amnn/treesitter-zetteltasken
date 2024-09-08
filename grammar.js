6bbb87f0882eebade922a36ac157d16d0c740d80
Ashok Menon <ashok@mystenlabs.com>
2024-09-08 18:09:33 +0100

    feat: integrate treesitter-markdown  (#2)
    Derive our grammar from the markdown one. This requires pulling in
    `treesitter-markdown` as a submodule so that we can also pull in the
    external scanner.
    
    Note that this probably breaks all the bindings which now need to
    include the scanners' source code. This will be fixed in a follow-up PR.

--- grammar.js
+++ grammar.js
@@ -1,9 +1,7 @@
 /// <reference types="tree-sitter-cli/dsl" />
 // @ts-check
 
-module.exports = grammar({
+module.exports = grammar(require("tree-sitter-markdown/grammar"), {
   name: "zetteltasken",
-  rules: {
-    source_file: (_) => "hello",
-  },
+  rules: {},
 });
---
+++ src/scanner.c
@@ -0,0 +1,13 @@
+/** Re-use the markdown external scanner for zetteltasken. */
+#define tree_sitter_markdown_external_scanner_create                           \
+  tree_sitter_zetteltasken_external_scanner_create
+#define tree_sitter_markdown_external_scanner_destroy                          \
+  tree_sitter_zetteltasken_external_scanner_destroy
+#define tree_sitter_markdown_external_scanner_serialize                        \
+  tree_sitter_zetteltasken_external_scanner_serialize
+#define tree_sitter_markdown_external_scanner_deserialize                      \
+  tree_sitter_zetteltasken_external_scanner_deserialize
+#define tree_sitter_markdown_external_scanner_scan                             \
+  tree_sitter_zetteltasken_external_scanner_scan
+
+#include "../vendor/tree-sitter-markdown/tree-sitter-markdown/src/scanner.c"
